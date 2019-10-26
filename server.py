9c26b6d3698e234314859cf8152fa0808290dc63
Jared Flores <jared_flores@cs.ucsb.edu>
2019-10-25 22:20:38 -0700

    Fixing Heroku Stuff

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 import asyncio
 import websockets
+import os
 
 async def mmcbfbbr( websocket, path ):
 	async for msg in websocket:
 		await websocket.send( msg )
 
-start_server = websockets.serve( mmcbfbbr, "0.0.0.0", 1004 )
+start_server = websockets.serve( mmcbfbbr, "0.0.0.0",  )
 
-asyncio.get_event_loop().run_until_complete( start_server )
+asyncio.get_event_loop().run_until_complete( start_server, int( os.environ[ "PORT" ] ) )
 asyncio.get_event_loop().run_forever()
